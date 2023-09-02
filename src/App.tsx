74f9fc4b028803fa8283d36da93d540b6d8b17eb
Shane <simess@vcu.edu>
2023-09-02 16:53:39 -0400

    Code

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import Button from "./components/Button";
+import Alert from "./components/Alert";
+import { useState } from "react";
+
+function App() {
+  const [alertVisible, setAlertVisibility] = useState(false);
+
+  return (
+    <div>
+      {alertVisible && (
+        <Alert onClose={() => setAlertVisibility(false)}>My alert</Alert>
+      )}
+      <Button color="primary" onClick={() => setAlertVisibility(true)}>
+        First Button
+      </Button>
+    </div>
+  );
+}
+
+export default App;
